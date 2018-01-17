343a4ea0d2100c4bfdbc389e033dfa0cd09ea43b
RoksanaN <oksananekrut@gmail.com>
2018-01-17 21:19:04 +0200

    change store image name

--- scrap.php
+++ scrap.php
@@ -38,11 +38,17 @@ function scrap(string $articleUrl) {
     $targetDirectory = "result/$article";
     file_exists($targetDirectory) || mkdir($targetDirectory, 0777);
 
+    $count = 1;
+
     foreach ($imageUrlList as $imageUrl) {
         $imageBaseName = basename($imageUrl);
 
+        $extension = $ext = pathinfo($imageBaseName, \PATHINFO_EXTENSION);
+        $imageName = "{$article}_{$count}.{$extension}";
+        ++$count;
+
         $imageContent = file_get_contents($articleUrl . $imageUrl);
 
-        file_put_contents('./result/' . $article . '/' . $imageBaseName, $imageContent);
+        file_put_contents('./result/' . $article . '/' . $imageName, $imageContent);
     }
 }
